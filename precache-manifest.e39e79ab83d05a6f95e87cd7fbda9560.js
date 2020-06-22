a161ae576fa0d79d81b804b165c4a6d217887d8c
Alexander <1alexvash@gmail.com>
2020-06-22 17:30:28 +0300

    Updates

---
+++ precache-manifest.e39e79ab83d05a6f95e87cd7fbda9560.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd4e8d610653b3c49af273f5ba335465",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca804b5f91eb3c9aa8e7",
+    "url": "/static/css/main.7e172274.chunk.css"
+  },
+  {
+    "revision": "61244f977ab1777e6608",
+    "url": "/static/js/2.76363ced.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.76363ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca804b5f91eb3c9aa8e7",
+    "url": "/static/js/main.ddcd7edf.chunk.js"
+  },
+  {
+    "revision": "e768b08d89dad30d4aa1",
+    "url": "/static/js/runtime-main.689802b0.js"
+  },
+  {
+    "revision": "e2148c85e91794f4dc01bf7d84d33fde",
+    "url": "/static/media/close-mark.e2148c85.png"
+  },
+  {
+    "revision": "6207b3dcad83057a616d0a72e3a66888",
+    "url": "/static/media/question-mark.6207b3dc.png"
+  },
+  {
+    "revision": "7768c1d08a2ccb9be8d865eb81b94993",
+    "url": "/static/media/sound_failure.7768c1d0.mp3"
+  },
+  {
+    "revision": "dcbb5b439d236a873f337fc600cf260c",
+    "url": "/static/media/sound_success.dcbb5b43.mp3"
+  }
+]);
