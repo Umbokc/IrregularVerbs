272e6822074d35e476c6534c5e3ea761c40e31df
Alexander <1alexvash@gmail.com>
2020-06-22 17:32:04 +0300

    Updates

---
+++ precache-manifest.af24a6bc979834a5c717d2e74a76150c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e02c1e9d922d390bd66aaa764d5f42",
+    "url": "/IrregularVerbs/index.html"
+  },
+  {
+    "revision": "bee090e5293ed1499325",
+    "url": "/IrregularVerbs/static/css/main.7e172274.chunk.css"
+  },
+  {
+    "revision": "61244f977ab1777e6608",
+    "url": "/IrregularVerbs/static/js/2.76363ced.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IrregularVerbs/static/js/2.76363ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee090e5293ed1499325",
+    "url": "/IrregularVerbs/static/js/main.6b6e8638.chunk.js"
+  },
+  {
+    "revision": "a9c9bc6cce204a52cf94",
+    "url": "/IrregularVerbs/static/js/runtime-main.99905a05.js"
+  },
+  {
+    "revision": "e2148c85e91794f4dc01bf7d84d33fde",
+    "url": "/IrregularVerbs/static/media/close-mark.e2148c85.png"
+  },
+  {
+    "revision": "6207b3dcad83057a616d0a72e3a66888",
+    "url": "/IrregularVerbs/static/media/question-mark.6207b3dc.png"
+  },
+  {
+    "revision": "7768c1d08a2ccb9be8d865eb81b94993",
+    "url": "/IrregularVerbs/static/media/sound_failure.7768c1d0.mp3"
+  },
+  {
+    "revision": "dcbb5b439d236a873f337fc600cf260c",
+    "url": "/IrregularVerbs/static/media/sound_success.dcbb5b43.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e39e79ab83d05a6f95e87cd7fbda9560.js"
+  "/IrregularVerbs/precache-manifest.af24a6bc979834a5c717d2e74a76150c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IrregularVerbs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
